9c5fb2367e257d3d6696e152540e0acc2fe2fbda
Gayathri11-2023 <150446843+Gayathri11-2023@users.noreply.github.com>
2024-02-12 21:35:54 +0530

    Task21 Added

---
+++ cookie.py
@@ -0,0 +1,45 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from time import sleep
+
+class loginPage:
+    # constructor method
+    def __init__(self, url):
+        self.url = url
+        self.driver = webdriver.Chrome(service = Service(ChromeDriverManager().install()))
+    # Get the url and boot the driver
+    def boot(self):
+        self.driver.get(self.url)
+        self.driver.maximize_window()
+        sleep(5)
+   # Quit from the driver
+    def quit(self):
+        self.driver.quit()
+    # Login into url site
+    def login(self):
+        username_input = self.driver.find_element(by=By.ID, value="user-name")
+        password_input = self.driver.find_element(by=By.ID,  value="password")
+        username_input.send_keys("standard_user")
+        password_input.send_keys("secret_sauce")
+        self.driver.find_element(by=By.ID, value= "login-button").click()
+        sleep(5)
+    # Get the cookies from the driver
+    def getCookies(self):
+        return self.driver.get_cookies()
+    # get the  Current url
+    def getCurrent_URL(self):
+        return self.driver.current_url
+
+url = "https://www.saucedemo.com/"
+obj = loginPage(url)
+obj.boot()
+print(obj.getCookies())
+print(obj.getCurrent_URL())
+obj.login()
+print(obj.getCookies())
+obj.quit()
+
+
+
